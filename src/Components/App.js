206d4e40581a5b251d8c5302b65ee795eb60c2a6
Abdul Ibrahim <theoneadisa@gmail.com>
2021-10-14 04:04:00 +0100

    add functionalityy to Dropdown bar

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import ReactBootstrap from 'react-bootstrap';
+// import ReactBootstrap from 'react-bootstrap';
+import 'react-pro-sidebar/dist/css/styles.css';
+// import { ProSidebar } from 'react-pro-sidebar';
+import ProSidebars from './ProSidebar';
 
 function App(){
 return(
-    <h1 className='container'>Hello World</h1>
+    <><h1>My Website</h1><ProSidebars /></>
+
 )
 }
 
---
+++ src/Components/ProSidebar.tsx
@@ -0,0 +1,19 @@
+import { Menu, MenuItem, ProSidebar, SubMenu } from 'react-pro-sidebar';
+import 'react-pro-sidebar/dist/css/styles.css';
+
+
+function ProSidebars(){
+    return(
+ <ProSidebar>
+  <Menu iconShape="square">
+    <MenuItem>Dashboard</MenuItem>
+    <SubMenu title="Components">
+      <MenuItem>Component 1</MenuItem>
+      <MenuItem>Component 2</MenuItem>
+    </SubMenu>
+  </Menu>
+</ProSidebar>
+    )
+}
+
+export default ProSidebars;
