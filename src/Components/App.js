6f5274fe6a0e20992379f63d83416ad5367b80f4
Abdul Ibrahim <theoneadisa@gmail.com>
2021-10-16 03:44:23 +0100

    second navbar

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,10 +3,13 @@ import React from 'react';
 import 'react-pro-sidebar/dist/css/styles.css';
 // import { ProSidebar } from 'react-pro-sidebar';
 import ProSidebars from './ProSidebar';
+import Navbars from './Navbar';
+// import { Navbar } from 'react-bootstrap';
 
 function App(){
 return(
-    <><h1>My Website</h1><ProSidebars /></>
+    <><h1>My Website</h1><ProSidebars />
+    <Navbars/></>
 
 )
 }
