d00e71879ee9fbb73b340635ddf03392c981c660
Abdul Ibrahim <theoneadisa@gmail.com>
2021-10-26 05:13:58 +0100

    some changes

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,14 +4,18 @@ import 'react-pro-sidebar/dist/css/styles.css';
 // import { ProSidebar } from 'react-pro-sidebar';
 import ProSidebars from './ProSidebar';
 import Navbars from './Navbar';
+import Sidebar from './Sidebar';
 // import { Navbar } from 'react-bootstrap';
 
 function App(){
 return(
-    <><h1>My Website</h1><ProSidebars />
+    <><h1>My Website</h1>
+    {/* <Sidebar/> */}
+    <ProSidebars />
     <Navbars/>
-    <p> this is an online store containing all of your favourite products.</p></>
-
+    <p> this is an online store containing all of your favourite products.</p>
+    
+    </>
 )
 }
 
